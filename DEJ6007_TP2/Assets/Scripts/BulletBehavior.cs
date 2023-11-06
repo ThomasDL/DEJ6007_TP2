11e4987f8b4505315cf63e9f2a601887443f899a
ThomasDL <thomas.dagenaisl@gmail.com>
2023-11-06 14:56:23 -0500

    Bullets! GroundCheck!

---
+++ DEJ6007_TP2/Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(AutoDestroy());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator AutoDestroy()
+    {
+        yield return new WaitForSeconds(3f);
+        Destroy(gameObject);
+    }
+}
--- DEJ6007_TP2/Assets/Scripts/PlayerMovement.cs
+++ DEJ6007_TP2/Assets/Scripts/PlayerMovement.cs
@@ -5,39 +5,52 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
+    public Camera cam;
+
+    public GameObject bulletPrefab;
+    public Transform gunPoint;
+
+    public Transform groundChecker;
+    float groundDistance = 0.58f;
 
     float speed = 12f;
-    float gravity = -20f;
-    float jumpForce = 10f;
+    float gravity = -25f;
+    float jumpForce = 15f;
 
-    public Vector3 velocity;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    Vector3 velocity;
+ 
     void Update()
     {
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
-
         controller.Move(move * speed * Time.deltaTime);
 
-        if (IsGrounded()) velocity.y = 0f;
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        bool isGrounded = IsGrounded();
+        if (isGrounded) velocity.y = 0f;
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = jumpForce;
         }
 
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+        if (Input.GetMouseButtonDown(0))
+        {
+            ShootBullet();
+        }
     }
     bool IsGrounded()
     {
-        return Physics.CheckSphere(transform.position - new Vector3(0, controller.height * 0.5f - controller.radius*0.8f, 0), controller.radius*0.95f, LayerMask.GetMask("Ground"));
+        return Physics.CheckSphere(groundChecker.position, groundDistance, LayerMask.GetMask("Ground"));
+    }
+    void ShootBullet()
+    {
+        Debug.Log("Yeah");
+        float bulletForce = 50f;
+        Rigidbody bulletRigidBody = Instantiate(bulletPrefab, gunPoint.position, cam.transform.rotation).GetComponent<Rigidbody>();
+        bulletRigidBody.rotation = Quaternion.Euler(cam.transform.rotation.eulerAngles.x + 90, cam.transform.rotation.eulerAngles.y, cam.transform.rotation.eulerAngles.z);
+        bulletRigidBody.AddForce(bulletRigidBody.transform.forward * bulletForce, ForceMode.Impulse);
     }
 }
