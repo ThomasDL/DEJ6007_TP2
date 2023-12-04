25679eac7449120ec094e38b6808ef74536059c6
ThomasDL <thomas.dagenaisl@gmail.com>
2023-12-04 15:10:38 -0500

    Enemy Health Bars + Death Animation

--- Assets/Scripts/Bullets/ShotgunBulletBehavior.cs
+++ Assets/Scripts/Bullets/ShotgunBulletBehavior.cs
@@ -9,6 +9,7 @@ public class ShotgunBulletBehavior : MonoBehaviour
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground")) collisionCounter++;
-        if (collisionCounter == 2) Destroy(gameObject);
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemies")) collision.collider.GetComponentInParent<EnemyBase>().DamageEnemy(1);
+        if (collisionCounter == 1) Destroy(gameObject);
     }
 }
